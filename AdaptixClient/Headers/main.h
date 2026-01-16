112d17dff6aca8f39a3e0fc0e87f5fdc38afaa22
blackman <blackman@test.com>
2026-01-16 16:42:28 +0800

    版本升级到 v1.01

--- AdaptixClient/Headers/main.h
+++ AdaptixClient/Headers/main.h
@@ -67,8 +67,8 @@
 #include <Utils/FileSystem.h>
 #include <Utils/Convert.h>
 
-#define FRAMEWORK_VERSION "Always Online v1.0"
-#define SMALL_VERSION     "v1.0"
+#define FRAMEWORK_VERSION "Always Online v1.01"
+#define SMALL_VERSION     "v1.01"
 
 ///////////
 
